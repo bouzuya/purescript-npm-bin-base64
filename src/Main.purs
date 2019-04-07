3443703c930219c1734c479a0aa4b3673c7172aa
bouzuya <m@bouzuya.net>
2019-04-07 15:02:24 +0900

    add version implementation

--- src/Main.purs
+++ src/Main.purs
@@ -8,10 +8,13 @@ import Effect (Effect)
 import Effect.Console as Console
 import Options (Options)
 import Options as Options
+import Version as Version
 
 app :: Options -> Effect Unit
 app options = do
-  Console.logShow options
+  if options.version
+    then Console.log Version.version
+    else Console.logShow options
 
 main :: Effect Unit
 main = Options.parse >>= app
